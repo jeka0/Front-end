dd4baeb823621f9ed1a8a60f0a093b2c0ad2f0e1
jeka0 <latosh02Wmail.ru>
2022-10-26 17:49:05 +0300

    install

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+let HandelClick = ()=>alert("addsa");
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input type = {"text"} placeholder ={"login"}/>
+     <input type = {"text"} placeholder ={"password"}/>
+      <button onclick = "HandelClick">click me</button>
     </div>
   );
 }
