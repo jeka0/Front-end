6c28f905c33128147885cf3860aa09c2b75c986d
jeka0 <latosh02@mail.ru>
2022-11-07 18:41:53 +0300

    Added json sending

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
 import './App.css';
-let HandelClick = ()=>alert("addsa");
+import {useState} from 'react';
 function App() {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  let HandelClick = ()=>{
+    let json = {
+      "login": login,
+      "password": password
+    }
+    console.log( JSON.stringify(json) );
+  }
+  let updateLogin = (event)=>setLogin(event.target.value);
+  let updatePassword = (event)=>setPassword(event.target.value);
   return (
     <div className="App">
-    <input type = {"text"} placeholder ={"login"}/>
-     <input type = {"text"} placeholder ={"password"}/>
-      <button onClick ={HandelClick}>click me</button>
+      <div className="form">
+        <input type = {"text"} placeholder ={"login"} onInput = {updateLogin}/>
+        <input type = {"text"} placeholder ={"password"} onInput = {updatePassword}/>
+        <button onClick ={HandelClick}>Send</button>
+      </div>
     </div>
   );
 }
